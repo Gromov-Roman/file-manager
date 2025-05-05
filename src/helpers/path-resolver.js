425f13f0942326be62342c36f8cb3ea706b6df23
Roman Gromov <gromov.r.v.92@gmail.com>
2025-05-05 20:11:56 +0500

    Revert "feat: Implement file manager"
    This reverts commit 5248955a5c93c31401b499bba3c6e5c3274659ee.

--- src/helpers/path-resolver.js
+++
@@ -1,5 +0,0 @@
-import path from 'path';
-
-export function resolvePath(currentDir, sourcePath) {
-    return path.isAbsolute(sourcePath) ? sourcePath : path.resolve(currentDir, sourcePath);
-}
--- src/helpers/validators.js
+++
@@ -1,9 +0,0 @@
-import { Commands, OSFlags } from '../consts/enums.js';
-
-export function isValidCommand(cmd) {
-    return Object.values(Commands).includes(cmd);
-}
-
-export function isValidOSFlag(flag) {
-    return Object.values(OSFlags).includes(flag);
-}
